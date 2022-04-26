4ed6a8fe7906d33217221280eafcc44969c3a5fd
sanjay <ssah6694@gmail.com>
2022-04-26 16:24:47 +0530

    [sanjay]Added: Welcome File

---
+++ AddressBook_LINQ/AddressBook_LINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBook_LINQ
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the LINQ Program ");
+        }
+    }
+}
