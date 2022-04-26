458ee086e184c7adec10a364c3426a0c1be12157
sanjay <ssah6694@gmail.com>
2022-04-26 17:59:21 +0530

    [sanjay]Added: Address book table File

---
+++ AddressBook_LINQ/AddressBook_LINQ/AddressBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_LINQ
+{
+    public class AddressBook
+    {
+        //UC1 create new address 
+        DataTable table = new DataTable("AddressBook");
+    }
+}
